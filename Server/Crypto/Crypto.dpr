b60f146f6e2a0261feabad20c45797cb792ffcbd
Mario Rossi <veontomo@gmail.com>
2016-12-19 10:18:00 +0100

    feat: use the comand line switches

--- Server/Crypto/Crypto.dpr
+++ Server/Crypto/Crypto.dpr
@@ -7,33 +7,39 @@ uses
   System.SysUtils,
   Encrypt in 'Encrypt.pas';
 
+const
+  // switches for comand line inputs
+  LOGIN_SWITCH = 'l';
+  PASSWORD_SWITCH = 'p';
+  SALT_LENGHT_SWITCH = 's';
+
 var
   cipher: TEncrypt;
   encryptData: TEncryptData;
-  login, password: String;
+  login, password, saltLengthStr: String;
   saltLength: Integer;
 
 begin
   cipher := TEncrypt.Create();
-  try
-    if ParamCount >= 3 then
-    begin
-      login := paramstr(1);
-      password := paramstr(2);
-      saltLength := StrToInt(paramstr(3));
+  if FindCmdLineSwitch(LOGIN_SWITCH, login, False) And FindCmdLineSwitch(PASSWORD_SWITCH, password, False) And
+    FindCmdLineSwitch(SALT_LENGHT_SWITCH, saltLengthStr, False) then
+  begin
+    saltLength := StrToInt(saltLengthStr);
+    try
       encryptData := cipher.Encrypt(login, password, saltLength);
       Writeln('login -> "' + login + '"');
       Writeln('password -> "' + password + '"');
       Writeln('salt -> "' + encryptData.salt + '"');
       Writeln('hash -> "' + encryptData.hash + '"');
       encryptData.DisposeOf;
-    end
-    else
-      Writeln('No input string is given.');
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
-  end;
+    except
+      on E: Exception do
+        Writeln(E.ClassName, ': ', E.Message);
+    end;
+  end
+  else
+    Writeln('Usage:' + sLineBreak + ExtractFileName(paramstr(0)) + ' -' + LOGIN_SWITCH +
+      ' <login name> -' + PASSWORD_SWITCH + ' <password> -' + SALT_LENGHT_SWITCH + ' <salt length>');
   cipher.DisposeOf;
 
 end.

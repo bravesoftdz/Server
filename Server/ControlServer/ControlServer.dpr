7193c71013bba61a3e2fd01dc1da4f9834111480
Mario Rossi <veontomo@gmail.com>
2016-12-28 12:06:49 +0100

    refactor: avoid using very nested if-then blocks

--- Server/ControlServer/ControlServer.dpr
+++ Server/ControlServer/ControlServer.dpr
@@ -48,58 +48,63 @@ begin
   isServerUrlValid := False;
   isUserAuthFileValid := False;
 
-  if FindCmdLineSwitch(SERVER_URL_SWITCH, ServerUrl, False) AND
-    FindCmdLineSwitch(SERVER_PORT_SWITCH, ServerPortStr, False) AND
-    FindCmdLineSwitch(AUTH_SWITCH, UserAuthFile, False) then
-  begin
-    ServerPort := -1;
-    try
-      ServerPort := StrToInt(ServerPortStr);
-      IsServerPortValid := ServerPort > 0;
-    except
-      on E: Exception do
-      begin
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
-        Writeln('Invalid port number: ' + ServerPortStr, E.Message);
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-      end
-    end;
-    isUserAuthFileValid := TFile.Exists(UserAuthFile);
-    isServerUrlValid := NOT(ServerUrl.IsNullOrWhiteSpace());
-    if ServerPort > 0 then
-    begin
-      if TFile.Exists(UserAuthFile) then
-      begin
-        TControlServerController.Authentication := TFileBasedAuthentification.Create(UserAuthFile);
-        TControlServerController.RESTAdapter := TRESTAdapter<IRedirectServerProxy>.Create;
-        TControlServerController.WebResource := TControlServerController.RESTAdapter.Build
-          (ServerUrl, ServerPort);
-        TServerLauncher.RunAsConsole(TWebBaseController, TwbmMain);
-      end
-      else
-      begin
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
-        Writeln('File ' + UserAuthFile + ' is not found');
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-        TServerLauncher.EndServer;
-      end;
-    end
-    else
+  FindCmdLineSwitch(SERVER_URL_SWITCH, ServerUrl, False);
+  FindCmdLineSwitch(SERVER_PORT_SWITCH, ServerPortStr, False);
+  FindCmdLineSwitch(AUTH_SWITCH, UserAuthFile, False);
+
+  try
+    ServerPort := StrToInt(ServerPortStr);
+    IsServerPortValid := ServerPort > 0;
+  except
+    on E: Exception do
     begin
       SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
-      Writeln('Port number must be a positive one');
+      Writeln('Warning: port number: "' + ServerPortStr + '" is invalid.', E.Message);
       SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-    end;
+    end
+  end;
+  isUserAuthFileValid := TFile.Exists(UserAuthFile);
+  isServerUrlValid := NOT(ServerUrl.Trim.IsEmpty());
+
+  if NOT(isUserAuthFileValid) then
+  begin
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 16);
+    Writeln('Warning: file "' + UserAuthFile + '" is not found.');
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+  end;
+
+  if NOT(IsServerPortValid) then
+  begin
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    Writeln('Port number must be a positive one');
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+  end;
+
+  if isUserAuthFileValid AND isServerUrlValid AND IsServerPortValid then
+  begin
+    TControlServerController.Authentication := TFileBasedAuthentification.Create(UserAuthFile);
+    TControlServerController.RESTAdapter := TRESTAdapter<IRedirectServerProxy>.Create;
+    TControlServerController.WebResource := TControlServerController.RESTAdapter.Build(ServerUrl,
+      ServerPort);
+    TServerLauncher.RunAsConsole(TWebBaseController, TwbmMain);
   end
   else
   begin
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    Writeln('Please, provide valid arguments.');
     Writeln('Usage:' + sLineBreak + ExtractFileName(paramstr(0)) + ' ' + SWITCH_CHAR +
       SERVER_URL_SWITCH + ' <url> ' + SWITCH_CHAR + SERVER_PORT_SWITCH + ' <port> ' + SWITCH_CHAR +
       AUTH_SWITCH + ' <file>' + sLineBreak + 'where ' + sLineBreak +
       '<url> - url of the redirect server,' + sLineBreak +
       '<port> - port number of the redirect server,' + sLineBreak +
       '<file> - file name containing authorization data.');
-    TServerLauncher.EndServer;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    try
+      TServerLauncher.EndServer;
+    except
+      on E: Exception do
+        Writeln('Error when stopping the server: ' + E.Message);
+    end;
   end;
 
 end.
